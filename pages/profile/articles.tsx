653cf4279ccafa86a80429a117a5642aa46939e8
Dominik Klimczyk <50147354+wise1999@users.noreply.github.com>
2023-01-22 17:42:00 +0100

    prepared articles profile page

--- pages/profile/articles.tsx
+++ pages/profile/articles.tsx
@@ -1,13 +1,9 @@
-import { useSession } from "next-auth/react";
-
-function ProfileIndex() {
-  const { data: session } = useSession();
-
+function ProfileArticles() {
   return (
     <>
       <section className="min-h-screen bg-gray-50 flex">
         <div className="flex flex-col py-10 px-8">
-          <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Welcome {session?.user?.name}</h2>
+          <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Your Articles</h2>
           <p className="mb-20 text-lg text-gray-500">Comes directly from the desk of engineers, creators and managers at Skcript.</p>
         </div>
       </section>
@@ -15,6 +11,6 @@ function ProfileIndex() {
   )
 }
 
-ProfileIndex.layout = "Profile"
+ProfileArticles.layout = "Profile"
 
-export default ProfileIndex
+export default ProfileArticles
